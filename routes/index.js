588fa05d343e88fcb30880dcc8e104e7e61bb5a2
Fabriciano Carrasco <fabricianocarrasco@gmail.com>
2017-06-02 21:19:34 +0200

    Practica52

--- routes/index.js
+++ routes/index.js
@@ -21,7 +21,7 @@ router.param('quizId', quizController.load);
 
 router.get('/quizzes/randomplay', quizController.randomplay);
 router.get('/quizzes/randomnomore', quizController.randomnomore);
-router.get('/quizzes/randomcheck:quizId(\\d+)', quizController.check);
+router.get('/quizzes/randomcheck/:quizId(\\d+)', quizController.check);
 // Definición de rutas de /quizzes
 router.get('/quizzes',                     quizController.index);
 router.get('/quizzes/:quizId(\\d+)',       quizController.show);
